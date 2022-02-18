06b106c1c425e855f4533c16886486498e18892a
Aayush9811 <tyagi.aayush3@gmail.com>
2022-02-18 05:16:42 +0000

    Using Firestore DB

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 'use strict';
 
+// bring in firestore
+const Firestore = require("@google-cloud/firestore");
+
+// initialize Firestore and set project id from env var
+const firestore = new Firestore(
+    {
+        projectId: process.env.GOOGLE_CLOUD_PROJECT
+    }
+);
+
 // express is a nodejs web server
 // https://www.npmjs.com/package/express
 const express = require('express');
@@ -45,23 +55,25 @@ app.get('/version', (req, res) => {
 // mock events endpoint. this would be replaced by a call to a datastore
 // if you went on to develop this as a real application.
 app.get('/events', (req, res) => {
-    res.json(mockEvents);
+    //res.json(mockEvents);
+    getEvents(req, res);
 });
 
 // Adds an event - in a real solution, this would insert into a cloud datastore.
 // Currently this simply adds an event to the mock array in memory
 // this will produce unexpected behavior in a stateless kubernetes cluster. 
 app.post('/event', (req, res) => {
-    // create a new object from the json data and add an id
+      // create a new object from the json data and add an id
     const ev = { 
         title: req.body.title, 
         description: req.body.description,
         id : mockEvents.events.length + 1
      }
-    // add to the mock array
-    mockEvents.events.push(ev);
-    // return the complete array
-    res.json(mockEvents);
+    // this will create the Events collection if it does not exist
+    firestore.collection("Events").add(ev).then(ret => {
+        getEvents(req, res);
+    });
+
 });
 
 app.use((err, req, res, next) => {
@@ -75,5 +87,25 @@ const server = app.listen(PORT, () => {
     const port = server.address().port;
     console.log(`Events app listening at http://${host}:${port}`);
 });
+
+function getEvents(req, res) {
+    firestore.collection("Events").get()
+        .then((snapshot) => {
+            if (!snapshot.empty) {
+                const ret = { events: []};
+                snapshot.docs.forEach(element => {
+                    ret.events.push(element.data());
+                }, this);
+                console.log(ret);
+                res.json(ret);
+            } else {
+                 res.json(mockEvents);
+            }
+        })
+        .catch((err) => {
+            console.error('Error getting events', err);
+            res.json(mockEvents);
+        });
+};
 
 module.exports = app;
